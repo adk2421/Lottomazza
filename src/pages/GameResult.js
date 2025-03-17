431619d079ec5fb603c5267bcc753f40bf7fa517
adk2421 <adk2421@gmail.com>
2025-03-17 14:26:59 +0900

    modify: Change get data method

--- src/pages/GameResult.js
+++ src/pages/GameResult.js
@@ -1,19 +1,42 @@
-import httpUtils from "../utils/httpUtils";
+import React, { useState } from 'react';
+import { TextField, Button, Box } from "@mui/material";
+import lottoData from "../data/lotto.json";
+// import httpUtils from "../utils/httpUtils";
 
+/**
+ * 회차별 당첨번호 조회
+ * @returns 
+ */
 const GameResult = () => {
+	const [lottoInfo, setLottoInfo] = useState({}); // 로또 당첨정보 변수
 
+	// 입력한 회차에 맞는 로또 당첨정보 가져오기
 	const getGameResult = () => {
-		const data = {
-			method: 'getLottoNumber',
-			drwNo: '1162',
-		};
-		console.log(httpUtils.httpMethod('/common.do?', 'get', data));
+		const searchNum = document.getElementById('searchNum').value;
+		setLottoInfo(lottoData['game_' + searchNum]);
 	}
 
 	return (
 		<div>
 			<h2>회차별 당첨번호 조회</h2>
-			<button type="button" onClick={getGameResult}>조회</button>
+			<div>
+				<TextField id="searchNum" size="small" label="회차번호" defaultValue="1162" />
+				<Button variant="contained" onClick={getGameResult}>조회</Button>
+			</div>
+			<Box
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+				gap="15px"
+				padding="30px"
+			>
+				<div id="drwtNo1" class="ball">{lottoInfo.drwtNo1}</div>
+				<div id="drwtNo2" class="ball">{lottoInfo.drwtNo2}</div>
+				<div id="drwtNo3" class="ball">{lottoInfo.drwtNo3}</div>
+				<div id="drwtNo4" class="ball">{lottoInfo.drwtNo4}</div>
+				<div id="drwtNo5" class="ball">{lottoInfo.drwtNo5}</div>
+				<div id="drwtNo6" class="ball">{lottoInfo.drwtNo6}</div>
+			</Box>
 		</div>
 	)
 }
